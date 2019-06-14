e92f57b7f3f21b1de03e2828cf446fdc38601e45
jcassarly <jaredcassarly@case.edu>
2019-06-13 22:09:24 -0400

    added comments

--- babyrov_arduino/babyrov_arduino.ino
+++ babyrov_arduino/babyrov_arduino.ino
@@ -1,25 +1,34 @@
 #include <Servo.h>
 
-#define MOTOR_PIN 9
+#define THRUSTER_PIN 9
 #define MIN_VALUE 0
 #define MAX_VALUE 255
 #define SERIAL_WAIT_DELAY 10
+#define BAUD_RATE 9600
 
 Servo motor;
 void setup() {
-    Serial.begin(9600);
-    motor.attach(MOTOR_PIN);
-    motor.writeMicroseconds(MIN_VALUE);// put your setup code here, to run onc
+    Serial.begin(BAUD_RATE);
+
+    // setup the thruster
+    motor.attach(THRUSTER_PIN);
+
+    // turn off the thruster
+    motor.writeMicroseconds(MIN_VALUE);
 }
 
 void loop() {
+    // wait for something to come in the serial
     if (Serial.available()) {
         unsigned char value = Serial.read();
 
+        // check that the value received is valid
         if (value >= MIN_VALUE && value <= MAX_VALUE) {
+            // turn on the thruster to the read value
             motor.writeMicroseconds(value);
         }
     }
+    // wait a short period before checking the serial again
     else {
         delay(SERIAL_WAIT_DELAY);
     }
